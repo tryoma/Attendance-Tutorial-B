144bac19ab825ca7f23180ae796436f85f340ca5
tryoma <tryoma0326@gmail.com>
2020-03-09 12:37:16 +0000

    03091

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
   before_action :set_one_month, only: :show
+  before_action :admin_or_correct_user, only: [:show, :index, :update]
 
   
   def index
